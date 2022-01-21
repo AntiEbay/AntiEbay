3a8843994810b05f3ecdc1514d1b384e0447c0c2
Andrew <stallingswandrew@gmail.com>
2022-01-20 17:06:46 -0800

    updated nav bar and added logo

---
+++ front-end/src/Components/Elements/Logo.js
@@ -0,0 +1,9 @@
+import react from 'react'
+//Text that can be used as a logo
+const Logo = () =>  {
+    return (
+        <span className= ' tracking-tighter text-white font-bold italic border-solid border-x-neutral-700'>Anti-eBay</span>
+    );
+}
+
+export default Logo;
--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,16 +1,33 @@
 import React from "react";
+import Logo from "./Elements/Logo";
+/**
+ * Contains logo, SearchBar and three Right alligned buttons In that order. 
+ * todo - Make searchBar look better
+ *  
+ */
 const NavBar = () => {
     return (
-        <div className="flex flex-row-reverse space-x-4 space-x-reverse h-12 bg-slate-500 relative p-1">
-            <button class="bg-green-500 hover:bg-green-700 cursor-pointer text-white font-bold py-2 px-2 rounded">
-                Log In
-            </button>
-            <button class="bg-green-500 hover:bg-green-700 cursor-pointer text-white font-bold py-2 px-2 rounded">
-                Sign Up
-            </button>
-            <button class="bg-green-500 hover:bg-green-700 cursor-pointer text-white font-bold py-2 px-2 rounded">
+        <div className="flex justify-between space-x-4 h-12 relative p-1 bg-slate-800">
+            <div className="flex p-1.5">
+                <Logo/> 
+                <div className=" ml-4">      
+                    <input className="w-80 h-7 rounded-l-full shadow-lg outline-none
+                    font-thin text-xl px-4" placeholder="Search"/>                  
+                    <button className="h-7 w-28 text-white rounded-r-full text-xl
+                    font-thin space-x-0 hover:bg-slate-400">Search</button>
+                </div>
+            </div>
+            <div>
+            <button class="hover:bg-slate-400 cursor-pointer text-white font-bold py-2 px-2 rounded">
                 FAQ
             </button>
+            <button class="hover:bg-slate-400 cursor-pointer text-white font-bold py-2 px-2 rounded">
+                Log In
+            </button>
+            <button class="hover:bg-slate-400 cursor-pointer text-white font-bold py-2 px-2 rounded">
+                Sign Up
+            </button>
+            </div>
         </div>
     );
 };
