c769b26b3d780b1fac1b158393a0e821e631163a
adara672 <82904129+adara672@users.noreply.github.com>
2022-01-31 17:00:36 -0800

    Add files via upload
    Updated database. Separate tables for buyer and seller with the email address being the primary key (and foreign key).

--- back-end/database/dockerfolder/schema.sql
+++ back-end/database/dockerfolder/schema.sql
@@ -1,12 +1,34 @@
 CREATE DATABASE IF NOT EXISTS AntiEbay;
 USE AntiEbay;
 
+-- Table of all users. These include attributes shared by both buyer and seller
 CREATE TABLE IF NOT EXISTS user (
-    userId VARCHAR(225) NOT NULL PRIMARY KEY,
+    userId VARCHAR(225),
     firstName VARCHAR(255),
     lastName VARCHAR(255), 
-    emailAddress VARCHAR(255),
+    emailAddress VARCHAR(255) PRIMARY KEY,
     userType VARCHAR(225),  
     password CHAR(41),
     joinDate DATETIME
+);
+
+/*
+-- Table for user type, eacher a buyer or a seller. Will do the logic on Backedn service so this might not be nessesay. 
+CREATE TABLE IF NOT EXISTS userType (
+    userType VARCHAR(225) PRIMARY KEY,
+    type ENUM('buyer', 'seller'),
+    CONSTRAINT FK_userType Foreign Key (userType) REFERENCES user(userType)
+);
+*/
+
+-- Table for buyers. More attributes will be added as profile develops
+CREATE TABLE IF NOT EXISTS buyer (
+    emailAddress VARCHAR(225) PRIMARY KEY,
+    CONSTRAINT FK_emailAddress Foreign Key (emailAddress) REFERENCES user(emailAddress)
+);
+
+-- Table for seller. More attributes will be added as profile develops
+CREATE TABLE IF NOT EXISTS seller (
+    emailAddress VARCHAR(225) PRIMARY KEY,
+    CONSTRAINT FK_emailAddress Foreign Key (emailAddress) REFERENCES user(emailAddress)
 );
