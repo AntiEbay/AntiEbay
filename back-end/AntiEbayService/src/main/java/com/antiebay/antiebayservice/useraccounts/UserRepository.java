bdef461e6e71a77b1e1288f842f43af47e6559a9
AlexWorland <59108991+AlexWorland@users.noreply.github.com>
2022-02-02 12:22:51 -0800

    Added a query as a test.

--- back-end/AntiEbayService/src/main/java/com/antiebay/antiebayservice/useraccounts/UserRepository.java
+++ back-end/AntiEbayService/src/main/java/com/antiebay/antiebayservice/useraccounts/UserRepository.java
@@ -1,8 +1,10 @@
 package com.antiebay.antiebayservice.useraccounts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserAccount, Integer> {
+    void exampleQuery();
 }
