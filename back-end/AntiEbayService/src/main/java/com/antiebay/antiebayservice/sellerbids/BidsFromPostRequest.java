9eec36f7141de3df39fce0fd4369e1b69e968485
Fetchy77 <Nicholas.fechtel@cwu.edu>
2022-03-12 10:40:05 -0800

    Update BidsFromPostRequest.java
    Finished comments in the BidsFromPostRequest class

--- back-end/AntiEbayService/src/main/java/com/antiebay/antiebayservice/sellerbids/BidsFromPostRequest.java
+++ back-end/AntiEbayService/src/main/java/com/antiebay/antiebayservice/sellerbids/BidsFromPostRequest.java
@@ -3,10 +3,18 @@ package com.antiebay.antiebayservice.sellerbids;
 public class BidsFromPostRequest {
     private Integer postId;
 
+    /**
+     * Method that returns the objects postId
+     * @return postId
+     */
     public Integer getPostId() {
         return postId;
     }
 
+    /**
+     * Method that sets the integer that is past to the method and sets it as this objects postId
+     * @param postId
+     */
     public void setPostId(Integer postId) {
         this.postId = postId;
     }
